72e46ae533a0f41ac932f6d8029dcf391723c5cb
JeraldVin <46400789+JeraldVin@users.noreply.github.com>
2019-01-20 21:16:24 +0530

    Create begset214.java

---
+++ begset214.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Display
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int q=sc.nextInt();
+		n++;
+		q--;
+		if(n%2==0)
+		n++;
+		if(q%2==0)
+		q--;
+		for(int i=n;i<=q;i+=2)
+		System.out.print(i+" ");
+	}
+}
