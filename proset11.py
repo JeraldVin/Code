ecb95d1ebad9053631a706b40753d9d08f5e4232
JeraldVin <46400789+JeraldVin@users.noreply.github.com>
2019-02-01 14:17:49 +0530

    Update proset11.py

--- proset11.py
+++ proset11.py
@@ -3,16 +3,19 @@ string=[]
 flag=0
 for i in range(0,n):
 	string.append(input())
-for i in range(0,len(string[0])):
+for i in range(1,len(string[0])):
 	for j in range(0,len(string)-1):
-		if len(string[j])<=i or len(string[j+1])<=i:
-			flag=1
+		if len(string[j])<=i or len(string[j+1])<=i      :
+			flag=2
 			break
-		if(string[j][:i]==string[j+1][:i]):
+		if(string[j][:i+1]==string[j+1][:i+1]):
 			continue
 		else:
 			flag=1
 			break
-	if flag==1:
+	if flag==1 or flag==2:
 		break
-print(string[0][:i-1])
+if flag==2 or flag==1:
+	print(string[0][:i])
+elif flag==0:
+	print(string[0])
