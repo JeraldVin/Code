870a650a3d8c9aef0b6a2d27af0cf4d1218a96b2
JeraldVin <46400789+JeraldVin@users.noreply.github.com>
2019-02-04 19:50:35 +0530

    Update proset763.py

--- proset763.py
+++ proset763.py
@@ -4,19 +4,16 @@ def unique(s):
 			return -1
 	return 1
 string=input()
-j=0
 k=1
 length=0
 while(True):
-	ss=string[j:k]
+	ss=string[0:k]
 	res=unique(ss)
 	if(res>0):
-		if(len(ss)>length):
-			length=len(ss)
+		length+=1
 		k+=1
 	else:
-		j+=1
-		k=j+1
-	if k>len(string) or j>len(string):
+		break
+	if k>len(string):
 		break
 print(length)
