99cfd9e6692eaccba6f75267257ac713beb4784b
JeraldVin <46400789+JeraldVin@users.noreply.github.com>
2019-02-04 20:22:22 +0530

    Update huntset431.py

--- huntset431.py
+++ huntset431.py
@@ -1,24 +1,14 @@
 n=int(input())
 num=list(map(int,input().split(" ")))
-pos=[]
-neg=[]
-for i in range(0,len(num)):
-	if num[i]>0:
-		pos.append(num[i])
-	elif num[i]<0:
-		neg.append(num[i])
-count=0
-if len(neg)%2!=0:
-	count=len(neg)-1
-else:
-	count=len(neg)
-prod=1
-ne=0
-for i in range(0,len(num)):
-	if num[i]<0:
-		ne+=1
-	if num[i]!=0 and ne<=count:
-		prod*=num[i]
-	else:
-		break
-print(prod)
+j=0
+ans=1
+for i in range(0,n):
+	prod=1
+	for j in range(i,n):
+		if num[j]!=0:
+			prod*=num[j]
+			if prod>ans:
+				ans=prod
+		else:
+			break
+print(ans)
