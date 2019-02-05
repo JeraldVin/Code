10b9045eef7683062b70b8cc417d005641721957
JeraldVin <46400789+JeraldVin@users.noreply.github.com>
2019-02-05 21:26:31 +0530

    Update huntset17.py

--- huntset17.py
+++ huntset17.py
@@ -4,4 +4,8 @@ res=[]
 for i in range(0,n):
 	if (i%2==0 and arr[i]%2!=0) or (i%2!=0 and arr[i]%2==0):
 		res.append(arr[i])
-print(*res,end="",sep=" ")
+for i in range(0,len(res)):
+	if i==len(res)-1:
+		print(res[i],end="")
+	else:
+		print(res[i],end=" ")
