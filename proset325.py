a6d605ae0f42a232f85a634f2dcfc4f8e42bf2e1
JeraldVin <46400789+JeraldVin@users.noreply.github.com>
2019-02-21 20:47:39 +0530

    Update proset325.py

--- proset325.py
+++ proset325.py
@@ -1,16 +1,16 @@
 n=int(input())
 arr=list(map(int,input().split()))
 length=0
-cur=0
+cur=1
 for i in range(n-1):
 	if arr[i]<arr[i+1]:
 		cur+=1
 		if i==n-2:
-			if cur+1>length:
-				length=cur+1
+			if cur>length:
+				length=cur
 				
 	else:
-		if cur+1>length:
-			length=cur+1
-		cur=0
+		if cur>length:
+			length=cur
+		cur=1
 print(length)
